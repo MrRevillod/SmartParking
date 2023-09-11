04f1abc66002e9860a13aeed01e16fb0a628bea5
Tomas Curihual <tvcurihual@gmail.com>
2023-09-11 18:03:51 -0300

    creacion de middleware para validar roles

---
+++ Backend/src/middlewares/roles.mw.js
@@ -0,0 +1,45 @@
+import { MESSAGES } from "../utils/http.utils.js"
+import { userModel } from "../models/user.model.js"
+
+
+export const roleValidation = (roles) => async (req, res, next) => {
+
+    try {
+
+        const uid = req.user.uid
+        const user = await userModel.findById(uid)
+
+        console.log(user)
+
+        if (!user || !roles.includes(user.role)) {
+            throw { status: 401, message: MESSAGES.UNAUTHORIZED }
+        }
+
+        next()
+
+    } catch (error) {
+        res.status(error?.status || 500).json({ message: error?.message || MESSAGES.UNEXPECTED })
+    }
+}
+
+export const ownerValidation = async (req, res, next) => {
+
+    try {
+
+        const reqId = req.user.uid
+        const userId = req.params.id
+
+        const user = await userModel.findById(reqId)
+
+        if (!user || (user.role !== 'ADMIN_ROLE' && reqId !== userId)) {
+            throw { status: 401, message: MESSAGES.UNAUTHORIZED }
+        }
+
+        req.user = user
+
+        next()
+
+    } catch (error) {
+        res.status(error?.status || 500).json({ message: error?.message || MESSAGES.UNEXPECTED })
+    }
+}
