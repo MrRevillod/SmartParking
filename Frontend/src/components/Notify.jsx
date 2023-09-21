7c04977e65902a8c63b1e06e5d3791f6fb85cb85
Carlito__ <106630043+SrCarlito@users.noreply.github.com>
2023-09-20 22:00:50 -0300

    fetching login/register

--- Frontend/src/components/Notify.jsx
+++ Frontend/src/components/Notify.jsx
@@ -1,46 +1,50 @@
+"use client";
 
-"use client"
-
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 const notificaciones = [
     { from: "carlos", message: "hola mundo", time: "12:00" },
     { from: "otro", message: "chaomundo", time: "13:00" },
-]
+];
 
 export default function Notify() {
-    const [noty, setNoty] = useState(false)
+    const [noty, setNoty] = useState(false);
 
     useEffect(() => {
+        const notyIcon = (e) => {
 
-        window.addEventListener("click", (e) => {
-            e.preventDefault()
-            if (e.target !== document.getElementById("noty-icon") && e.target !== document.getElementById("noty-dot")) {
-                console.log('click2')
-                setNoty(false)
+            if (
+                e.target !== document.getElementById("noty-icon") &&
+                e.target !== document.getElementById("noty-dot")
+            ) {
+                console.log("click2");
+                setNoty(false);
             }
-        })
+        };
 
-    }, [])
+        window.addEventListener("click", notyIcon,true);
+
+        return window.removeEventListener("click",notyIcon);
+
+    }, []);
 
     return (
-
         <>
             <div id="noty">
                 <i
                     className=" position-absolute bi bi-envelope-exclamation-fill h2 text-light "
                     id="noty-icon"
                     onClick={(e) => {
-                        e.preventDefault
-                        setNoty(!noty)
+                        e.preventDefault;
+                        setNoty(!noty);
                     }}
                 ></i>
                 {notificaciones.length !== 0 && (
                     <i
                         onClick={(e) => {
-                            e.preventDefault
-                            console.log('click')
-                            setNoty(!noty)
+                            e.preventDefault;
+                            console.log("click");
+                            setNoty(!noty);
                         }}
                         className="position-absolute  bi bi-circle-fill text-danger  small notify-dot "
                         id="noty-dot"
@@ -48,16 +52,13 @@ export default function Notify() {
                 )}
 
                 {noty && (
-                    <ul
-                        className="list-group position-absolute noty-list "
-
-                    >
+                    <ul className="list-group position-absolute noty-list ">
                         {notificaciones.map(({ from, message, time }, i) => {
                             return (
                                 <li
                                     onClick={(e) => {
-                                        e.preventDefault
-                                        alert("redirecting..")
+                                        e.preventDefault;
+                                        alert("redirecting..");
                                     }}
                                     className="link-opacity-25-hover  list-group-item d-flex justify-content-between align-items-start"
                                     key={i}
@@ -70,11 +71,11 @@ export default function Notify() {
                                         {time}
                                     </span>
                                 </li>
-                            )
+                            );
                         })}
                     </ul>
                 )}
             </div>
         </>
-    )
+    );
 }
