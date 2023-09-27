3d35202b476edd7b9b245ef1325f74e3b1f4603c
Tomas Curihual <tvcurihual@gmail.com>
2023-09-26 22:44:02 -0300

    logout

--- Backend/src/middlewares/session.mw.js
+++ Backend/src/middlewares/session.mw.js
@@ -2,12 +2,16 @@
 import { JWT_SECRET } from "../config/env.js"
 import { MESSAGES } from "../utils/http.utils.js"
 import { verifyJwt } from "../utils/jwt.utils.js"
+import { expiredTokens } from "../utils/etoken.utils.js"
 
 export const sessionValidation = async (req, res, next) => {
 
     try {
 
         const token = req.headers.authorization?.split(' ').pop() || ''
+
+        if ( expiredTokens.includes(token)) throw { status: 401, message: MESSAGES.UNAUTHORIZED }
+        
         const payload = verifyJwt(token, JWT_SECRET)
 
         req.user = payload
