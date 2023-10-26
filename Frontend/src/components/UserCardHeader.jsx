4f899b29e70562f0ab3b8d5af7c44aca02032618
Carlito__ <106630043+SrCarlito@users.noreply.github.com>
2023-10-26 11:56:22 -0300

    fix: ReactQuery x fetch

--- Frontend/src/components/UserCardHeader.jsx
+++ Frontend/src/components/UserCardHeader.jsx
@@ -18,37 +18,20 @@ export const UserCardHeader = ({ user, setIsModalVisible }) => {
 
             <div className=" back-blue row h-75 row " />
             <div className=" row w-100 m-0  h-25">
-                {/* <div className="col   align-content-center row w-100 m-0 ">{user.active ?
-                    <Pill bakgroundClass="bg-success ">Activo</Pill> :
-                    <Pill bakgroundClass="bg-danger ">Inactivo</Pill>
-                }</div> */}
                 <div className="col text-end  align-content-center row w-100 m-0 ">
                     <Pill bakgroundClass={"back-blue"}>
                         <div className="fs-4 px-3">{user.active ? user.parking : "--"}</div>
                     </Pill>
                 </div>
             </div>
-            {/* <div className="position-absolute bottom-0    w-100  row m-0 justify-content-center"
-            >
-                <div
-                    style={{ width: '30vh', height: '30vh' }}
-                    className="bg-black rounded-circle"
-                >   
-                </div>
-            </div> */}
-            <div className="position-absolute bottom-0  w-100  row m-0 justify-content-center"
-            >
+            <div className="position-absolute bottom-0  w-100  row m-0 justify-content-center">
                 <img src={user.profilePicture}
                     className="rounded-circle  pb-3"
                     style={imageStyle}
                     alt="" />
             </div>
-
-
-
-
-            <div className="position-absolute text-end w-100 h-100 pt-2 pe-3 top-0">
-                <i onClick={onClickHandler} className="bi bi-x-circle-fill h1 text-shadow text-light  "></i>
+            <div className="position-absolute text-end w-100 h-100 pt-2 pe-3 top-0 ">
+                <i onClick={onClickHandler} className="close-button opacity-75 bi bi-x-circle-fill h1 text-shadow text-light"/>
             </div>
         </section>
     )
