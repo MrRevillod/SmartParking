ce7a1d49a1a7f183cce6e314967b303ee8991d88
MrRevillod <lrevillod2022@alu.uct.cl>
2023-10-25 21:59:16 -0300

    feat: mongodb docker auth

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,7 +1,7 @@
 
 import { connect } from "mongoose"
-import { DB_NAME, MDB_PROD_URI, MDB_LOCAL_URI, MDB_DOCKER_URI, MODE } from "./env.js"
 import { MESSAGES } from "../utils/http.utils.js"
+import { DB_NAME, MDB_PROD_URI, MDB_LOCAL_URI, MDB_DOCKER_URI, MODE } from "./env.js"
 
 const URIS = {
     "DOCKER": MDB_DOCKER_URI,
@@ -13,6 +13,8 @@ const URI = URIS[MODE]
 
 export const dbConnect = async () => {
 
+    console.log(URI)
+
     try {
         return await connect(URI, {
             useNewUrlParser: true,
@@ -21,6 +23,7 @@ export const dbConnect = async () => {
         })
 
     } catch (error) {
+        console.log(error)
         throw { status: 500, message: MESSAGES.UNEXPECTED }
     }
 }
